122e8b3af50e55b27daef941c5d9df55bcbdfa4d
Adel Razzok <lhp6.arazzok@gmail.com>
2024-12-04 16:58:28 +0100

    add: day_3 part_3 starting code

--- day_3/part_2/main.cpp
+++ day_3/part_2/main.cpp
@@ -1,3 +1,41 @@
-int main(int argc, char* argv[]) {
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <regex>
+
+using namespace std;
+
+int     get_line_result(const string &line) {
+    
+}
+
+void    handle_file(ifstream &file) {
+    string  line;
+    int     result = 0;
+
+    while (getline(file, line)) {
+        result += get_line_result(line);
+    }
+    cout << "Result: " << result << endl;
+}
+
+int     main(int argc, char **argv) {
+    ifstream    file;
+    string      line;
+
+    if (argc != 2) {
+        cerr << "Usage: " << argv[0] << " <input_file>" << endl;
+        return 1;
+    }
+
+    file.open(argv[1], ifstream::in);
+    if (!file.is_open()) {
+        cerr << "Error: cannot open file " << argv[1] << endl;
+        return 1;
+    }
+
+    handle_file(file);
+    file.close();
+
     return 0;
 }
