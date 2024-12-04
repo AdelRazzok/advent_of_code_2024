4565870298dbf72472f0dddcc2cde36dd0cfcfe7
Adel Razzok <lhp6.arazzok@gmail.com>
2024-12-04 10:38:40 +0100

    add: day 1 part 1, update: .gitignore

--- day_1/part_1/main.cpp
+++ day_1/part_1/main.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int main(int argc, char **argv) {
+void    handle_file(ifstream &file, string &line) {
+    vector<int> list_one, list_two;
+    size_t      pos;
+    int         result;
+
+    while (getline(file, line)) {
+        pos = line.find("   ");
+        list_one.push_back(stoi(line.substr(0, pos)));
+        list_two.push_back(stoi(line.substr(pos + 3)));
+    }
+    sort(list_one.begin(), list_one.end());
+    sort(list_two.begin(), list_two.end());
+
+    if (list_one.size() != list_two.size()) {
+        cerr << "Error: lists are not the same size" << endl;
+        exit(1);
+    }
+
+    for (size_t i = 0; i < list_one.size(); i++) {
+        result += abs(list_one[i] - list_two[i]);
+    }
+    cout << "Result: " << result << endl;
+}
+
+int     main(int argc, char **argv) {
     ifstream    file;
     string      line;
 
@@ -12,16 +37,11 @@ int main(int argc, char **argv) {
         cerr << "Usage: " << argv[0] << " <input_file>" << endl;
         return 1;
     }
-
     file.open(argv[1], ifstream::in);
     if (!file.is_open()) {
         cerr << "Error: cannot open file " << argv[1] << endl;
         return 1;
     }
-
-    while (getline(file, line)) {
-        cout << line << endl;
-    }
-
+    handle_file(file, line);
     return 0;
 }
