68035fcbba81188ea002eb9ff15fd392bcbb0abc
Adel Razzok <lhp6.arazzok@gmail.com>
2024-12-05 16:21:01 +0100

    update: not working part 2 code

--- day_3/part_2/main.cpp
+++ day_3/part_2/main.cpp
@@ -6,7 +6,33 @@
 using namespace std;
 
 int     get_line_result(const string &line) {
-    
+    regex           main_pattern(R"(mul\((\d{1,3}),(\d{1,3})\))");
+    regex           state_pattern(R"(do\(\)|don't\(\))");
+    sregex_iterator it(line.begin(), line.end(), main_pattern);
+    sregex_iterator end;
+    bool            is_calculating = true;
+    int             sum = 0;
+
+    while (it != end) {
+        smatch match = *it;
+        string instruction = match.str();
+
+        if (instruction == "do()") {
+            is_calculating = true;
+        } else if (instruction == "don't()") {
+            is_calculating = false;
+        } else {
+            smatch mul_match;
+            if (regex_match(instruction, mul_match, main_pattern)) {
+                int x = stoi(mul_match[1].str());
+                int y = stoi(mul_match[2].str());
+                if (is_calculating) {
+                    sum += x * y;
+                }
+            }
+        }
+        ++it;
+    }
 }
 
 void    handle_file(ifstream &file) {
